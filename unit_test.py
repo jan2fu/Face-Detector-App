2ec3e0c5e6c6f1d6e4c5f062c9508ad04ed364d0
LamaJ <jamalample@gmail.com>
2025-03-21 04:18:51 -0500

    Added Unit Tests

--- unit_test.py
+++ unit_test.py
@@ -1,3 +1,7 @@
+import cv2
+import os
+
+
 def test_cascade_classifier_initialization():
     classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     assert not classifier.empty(), "Failed to load haarcascade XML file"
@@ -10,4 +14,3 @@ def test_image_grayscale_conversion():
     img = cv2.imread('DR.jpg')
     grayscaled_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     assert grayscaled_image is not None, "Image could not be converted to grayscale"
-    
